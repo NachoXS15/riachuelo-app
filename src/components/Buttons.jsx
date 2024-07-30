12e02ec3323422460949993c4d1bd596e5530531
NachoXS-NET <pantojaignacio642@gmail.com>
2024-07-29 22:51:05 -0300

    created buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,44 +2,48 @@ import { Link } from 'expo-router'
 import React from 'react'
 import { Pressable, Text, StyleSheet } from 'react-native'
 
-export default function ButtonVar({ children, variant = 'primary', to }) {
-    const selectedVariantStyle = btnVariants[variant] || btnVariants.primary;
-    
-    return (
-      <Link asChild href={to}>
-        <Pressable style={({ pressed }) => [
-          {
-            backgroundColor: pressed ? 'gray' : "#3b82f6",
-            paddingVertical: selectedVariantStyle.paddingVertical,
-            paddingHorizontal: selectedVariantStyle.paddingHorizontal,
-            borderRadius: selectedVariantStyle.borderRadius,
-          }
-        ]}>
-          <Text style={{fontSize: 16, textAlign: 'center' }}>{selectedVariantStyle.backgroundColor}</Text>
-        </Pressable>
-      </Link>
-    );
-  }
+export function ButtonPrimary({ children, to }) {
+  return (
+    <Link asChild href={to}>
+      <Pressable style={{
+          backgroundColor: "#43259f",
+          paddingVertical: 5,
+          paddingHorizontal: 10,
+          borderRadius: 50
+        }}>
+        <Text style={{ fontSize: 16, textAlign: 'center', color: 'white' }}>{children}</Text>
+      </Pressable>
+    </Link>
+  );
+}
 
-const btnVariants = StyleSheet.create({
-    primary: {
-        backgroundColor: '#3b82f6',
-        hoverBackgroundColor: '#1e40af',
-        color: '#ffffff',
-    },
-    secondary: {
-        backgroundColor: '#6b7280',
-        hoverBackgroundColor: '#374151',
-        color: '#ffffff',
-    },
-    success: {
-        backgroundColor: '#10b981',
-        hoverBackgroundColor: '#047857',
-        color: '#ffffff',
-    },
-    danger: {
-        backgroundColor: '#ef4444',
-        hoverBackgroundColor: '#b91c1c',
-        color: '#ffffff',
-    },
-})
+export function ButtonDanger({ children, to }) {
+  return (
+    <Link asChild href={to}>
+      <Pressable style={{
+          backgroundColor: "#931a1a",
+          paddingVertical: 5,
+          paddingHorizontal: 10,
+          borderRadius: 50
+        }}>
+        <Text style={{ fontSize: 16, textAlign: 'center', color: 'white' }}>{children}</Text>
+      </Pressable>
+    </Link>
+  );
+}
+
+export function ButtonGray({ children, to }) {
+  return (
+    <Link asChild href={to}>
+      <Pressable style={{
+          backgroundColor: "#b3b0c6",
+          paddingVertical: 5,
+          paddingHorizontal: 10,
+          borderRadius: 50
+        }}>
+        <Text style={{ fontSize: 16, textAlign: 'center', color: '#000' }}>{children}</Text>
+      </Pressable>
+    </Link>
+  );
+}
+
